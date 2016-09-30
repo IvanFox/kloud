038f7bcb70352b8a3044762ab475db193cd757bf
IvanFox <zvezdo4eet@gmail.com>
2016-09-30 20:14:23 +0100

    UnitTest class introduced

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,144 +1,149 @@
 import me.ivanlis.kloud.serialisation.SerialisationWriter
+import org.junit.Assert
+import org.junit.Test
 
 /**
  * Created by ivanlis on 28/09/2016.
  * Student Course: Software Development
  */
 
-fun main(args: Array<String>) {
-//    writeBytesWithByteValue()
-//    writeBytesWithShortValue()
-//    writeBytesWithIntegerValue()
-//    writeBytesWithLongValue()
-//    writeBytesWithFloatValue()
-//    writeBytesWithDoubleValue()
-    writeBytesWithBooleanValue()
-}
-
-fun writeBytesWithByteValue() {
-    val writer = SerialisationWriter()
-
-    val data = ByteArray(16)
-
-    val number = 10.toByte()
-
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
+class WriteToByteArrayTest {
 
 
-//  println(pointer)
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt()).capitalize()}")
-    })
-}
+    @Test fun writeBytesWithByteValue() {
+        val writer = SerialisationWriter()
 
-fun writeBytesWithShortValue() {
-    val writer = SerialisationWriter()
+        val data = ByteArray(16)
 
-    val data = ByteArray(16)
+        val number = 10.toByte()
 
-    val number = 10000.toShort()
-
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt())} ")
-    })
-}
-
-fun writeBytesWithIntegerValue(){
-    val writer = SerialisationWriter()
-
-    val data = ByteArray(16)
-
-    val number = 10000.toInt()
-
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-//    pointer = writer.writeBytes(data, pointer, number)
-
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt())} ")
-    })
-}
-
-fun writeBytesWithLongValue(){
-    val writer = SerialisationWriter()
-
-    val data = ByteArray(16)
-
-    val number = Long.MIN_VALUE
-
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
 
 
-    println(data.forEach {
-        print("0x${java.lang.Integer.toHexString(it.toInt())} ")
-    })
-}
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 5)
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt()).capitalize()}")
+        })
+    }
+
+    @Test fun writeBytesWithShortValue() {
+        val writer = SerialisationWriter()
+
+        val data = ByteArray(16)
+
+        val number = 10000.toShort()
+
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 10)
+
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt())} ")
+        })
+    }
+
+    @Test fun writeBytesWithIntegerValue() {
+        val writer = SerialisationWriter()
+
+        val data = ByteArray(16)
+
+        val number = 10000.toInt()
+
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 16)
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt())} ")
+        })
+    }
+
+    @Test fun writeBytesWithLongValue() {
+        val writer = SerialisationWriter()
+
+        val data = ByteArray(16)
+
+        val number = Long.MIN_VALUE
+
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 16)
+        println(data.forEach {
+            print("0x${java.lang.Integer.toHexString(it.toInt())} ")
+        })
+    }
 
 
-fun writeBytesWithFloatValue(){
-    val writer = SerialisationWriter()
+    @Test fun writeBytesWithFloatValue() {
+        val writer = SerialisationWriter()
 
-    val data = ByteArray(16)
+        val data = ByteArray(16)
 
-    val number = 1.1f
+        val number = 1.1f
 
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
-    pointer = writer.writeBytes(data, pointer, number)
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
+        pointer = writer.writeBytes(data, pointer, number)
 
-    println("Pointer points to: $pointer")
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt())} ")
-    })
-}
-
-
-fun writeBytesWithDoubleValue(){
-    val writer = SerialisationWriter()
-
-    val data = ByteArray(16)
-
-    val number = 1.1
-
-    var pointer = writer.writeBytes(data, 0, number)
-    pointer = writer.writeBytes(data, pointer, number)
-
-    println("Pointer points to: $pointer")
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt())} ")
-    })
-}
-
-fun writeBytesWithBooleanValue(){
-    val writer = SerialisationWriter()
-
-    val data = ByteArray(16)
-
-    val value1 = false
-    val value2 = true
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 16)
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt())} ")
+        })
+    }
 
 
-    var pointer = writer.writeBytes(data, 0, value2)
-    pointer = writer.writeBytes(data, pointer, value2)
-    pointer = writer.writeBytes(data, pointer, value1)
-    pointer = writer.writeBytes(data, pointer, value2)
+    @Test fun writeBytesWithDoubleValue() {
+        val writer = SerialisationWriter()
 
-    println("Pointer points to: $pointer")
-    println(data.forEach {
-        print("0x${Integer.toHexString(it.toInt())} ")
-    })
+        val data = ByteArray(16)
+
+        val number = 1.1
+
+        var pointer = writer.writeBytes(data, 0, number)
+        pointer = writer.writeBytes(data, pointer, number)
+
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 16)
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt())} ")
+        })
+    }
+
+    @Test fun writeBytesWithBooleanValue() {
+        val writer = SerialisationWriter()
+
+        val data = ByteArray(16)
+
+        val value1 = false
+        val value2 = true
+
+
+        var pointer = writer.writeBytes(data, 0, value2)
+        pointer = writer.writeBytes(data, pointer, value2)
+        pointer = writer.writeBytes(data, pointer, value1)
+        pointer = writer.writeBytes(data, pointer, value2)
+
+        println("Pointer points to: $pointer")
+        Assert.assertEquals(pointer, 4)
+        println(data.forEach {
+            print("0x${Integer.toHexString(it.toInt())} ")
+        })
+    }
 }
