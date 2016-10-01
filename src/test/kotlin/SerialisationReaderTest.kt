5664962b93a08d7cd999c87fa73f1ea964adff8b
IvanFox <zvezdo4eet@gmail.com>
2016-10-01 17:33:52 +0100

    unit testing for byte, shor, int ,long readers added

--- src/test/kotlin/SerialisationReaderTest.kt
+++ src/test/kotlin/SerialisationReaderTest.kt
@@ -8,14 +8,38 @@ import org.junit.Test
  * Student Course: Software Development
  */
 class SerialisationReaderTest {
+    val writer = SerialisationWriter()
+    val reader = SerialisationReader()
+
+    @Test fun testReadByte() {
+        val data = ByteArray(1)
+        val value = 6.toByte()
+        writer.writeBytes(data, 0 , value)
+        Assert.assertEquals(reader.readByte(data, 0), value)
+    }
+
+    @Test fun testReadShort() {
+        val data = ByteArray(2)
+        val value = 6.toShort()
+        writer.writeBytes(data, 0 , value)
+        Assert.assertEquals(reader.readShort(data, 0), value)
+    }
 
     @Test fun testReadInt() {
-        val writer = SerialisationWriter()
-        val reader = SerialisationReader()
         val data = ByteArray(4)
         val value = 6
         writer.writeBytes(data, 0 , value)
         Assert.assertEquals(reader.readInt(data, 0), value)
-
     }
+
+    @Test fun testReadLong() {
+        val data = ByteArray(8)
+        val value = 6.toLong()
+        writer.writeBytes(data, 0 , value)
+        Assert.assertEquals(reader.readLong(data, 0), value)
+    }
+
+
+
+
 }
