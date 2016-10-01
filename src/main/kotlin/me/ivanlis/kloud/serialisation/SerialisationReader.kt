aaa03b849b1d5311349f8d8c84aeef8c2e2ee0f7
IvanFox <zvezdo4eet@gmail.com>
2016-10-01 17:32:58 +0100

    byte, short, int, long readers added

--- src/main/kotlin/me/ivanlis/kloud/serialisation/SerialisationReader.kt
+++ src/main/kotlin/me/ivanlis/kloud/serialisation/SerialisationReader.kt
@@ -6,11 +6,30 @@ package me.ivanlis.kloud.serialisation
  */
 class SerialisationReader {
 
+    fun readByte(src: ByteArray, pointer: Int): Byte {
+        return src[pointer].toByte()
+    }
+
+    fun readShort(src: ByteArray, pointer: Int): Short {
+        return src[pointer].toInt().shl(8).or(src[pointer + 1].toInt()).toShort()
+    }
+
     fun readInt(src: ByteArray, pointer: Int): Int {
         return src[pointer].toInt().shl(24).
                 or(src[pointer + 1].toInt().shl(16)).
                 or(src[pointer + 2].toInt().shl(8)).
                 or(src[pointer + 3].toInt())
+    }
+
+    fun readLong(src: ByteArray, pointer: Int) : Long {
+        return src[pointer].toInt().shl(56).
+                or(src[pointer + 1].toInt().shl(48)).
+                or(src[pointer + 2].toInt().shl(40)).
+                or(src[pointer + 3].toInt().shl(32)).
+                or(src[pointer + 4].toInt().shl(24)).
+                or(src[pointer + 5].toInt().shl(16)).
+                or(src[pointer + 6].toInt().shl(8)).
+                or(src[pointer + 7].toInt()).toLong()
     }
 
 }
