d82ad81716b1b026cf31ce6f2238520c918e2e4b
IvanFox <zvezdo4eet@gmail.com>
2016-10-11 15:55:02 +0100

    Test case updated

--- src/test/kotlin/FieldTest.kt
+++ src/test/kotlin/FieldTest.kt
@@ -1,7 +1,8 @@
 import me.ivanlis.kloud.serialisation.ContainerType
 import me.ivanlis.kloud.serialisation.Field
 import me.ivanlis.kloud.serialisation.Type
-import me.ivanlis.kloud.serialisation.extensions.*
+import me.ivanlis.kloud.serialisation.extensions.printHex
+import me.ivanlis.kloud.serialisation.extensions.toHex
 import org.junit.Assert
 import org.junit.Test
 
@@ -19,9 +20,9 @@ class FieldTest {
         Assert.assertEquals("Container type should be: ", ContainerType.FIELD, data[0])
         Assert.assertEquals("Name size should be: ", "ages".length, data[2].toHex.toInt())
         Assert.assertEquals("ASCII code for a is :", 61, data[3].toHex.toInt())
-        Assert.assertEquals("ASCII code for g is :",67, data[4].toHex.toInt())
-        Assert.assertEquals("ASCII code for e is :",65, data[5].toHex.toInt())
-        Assert.assertEquals("ASCII code for s is :",73, data[6].toHex.toInt())
+        Assert.assertEquals("ASCII code for g is :", 67, data[4].toHex.toInt())
+        Assert.assertEquals("ASCII code for e is :", 65, data[5].toHex.toInt())
+        Assert.assertEquals("ASCII code for s is :", 73, data[6].toHex.toInt())
     }
 
     class IntField(name: String, value: Int) : Field(name) {
