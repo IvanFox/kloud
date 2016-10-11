ea54b173db75849b9845d22ec57f23042867b8fe
IvanFox <zvezdo4eet@gmail.com>
2016-10-11 16:54:09 +0100

    short, char and byte field tests added

--- src/test/kotlin/FieldTest.kt
+++ src/test/kotlin/FieldTest.kt
@@ -1,6 +1,4 @@
-import me.ivanlis.kloud.serialisation.ContainerType
-import me.ivanlis.kloud.serialisation.Field
-import me.ivanlis.kloud.serialisation.Type
+import me.ivanlis.kloud.serialisation.*
 import me.ivanlis.kloud.serialisation.extensions.printHex
 import me.ivanlis.kloud.serialisation.extensions.toHex
 import org.junit.Assert
@@ -12,6 +10,7 @@ import org.junit.Test
  */
 class FieldTest {
 
+    val reader = SerialisationReaderImpl()
 
     @Test fun testIntegerField() {
         val field = Field.Integer("ages", 5)
@@ -37,7 +36,53 @@ class FieldTest {
         Assert.assertEquals("ASCII code for l is :", "6c", data[4].toHex)
         Assert.assertEquals("ASCII code for a is :", "61", data[5].toHex)
         Assert.assertEquals("ASCII code for g is :", "67", data[6].toHex)
+        Assert.assertEquals("True is equal to :", 1.toByte(), data[8])
     }
 
+
+    @Test fun testByteField() {
+        val field = Field.Byte("myVar", 1)
+        val data = ByteArray(10)
+        field.getBytes(data, 0)
+        data.forEach { it.toInt().printHex() }
+        Assert.assertEquals("Container type should be: ", ContainerType.FIELD, data[0])
+        Assert.assertEquals("Name size should be: ", "myVar".length, data[2].toHex.toInt())
+        Assert.assertEquals("ASCII code for m is :", "6d", data[3].toHex)
+        Assert.assertEquals("ASCII code for y is :", "79", data[4].toHex)
+        Assert.assertEquals("ASCII code for V is :", "56", data[5].toHex)
+        Assert.assertEquals("ASCII code for a is :", "61", data[6].toHex)
+        Assert.assertEquals("ASCII code for r is :", "72", data[7].toHex)
+        Assert.assertEquals(1.toByte(), data[9])
+    }
+
+    @Test fun testCharField() {
+        val field = Field.Char("myVar", 'r')
+        val data = ByteArray(11)
+        field.getBytes(data, 0)
+        data.forEach { it.toInt().printHex() }
+        Assert.assertEquals("Container type should be: ", ContainerType.FIELD, data[0])
+        Assert.assertEquals("Name size should be: ", "myVar".length, data[2].toHex.toInt())
+        Assert.assertEquals("ASCII code for m is :", "6d", data[3].toHex)
+        Assert.assertEquals("ASCII code for y is :", "79", data[4].toHex)
+        Assert.assertEquals("ASCII code for V is :", "56", data[5].toHex)
+        Assert.assertEquals("ASCII code for a is :", "61", data[6].toHex)
+        Assert.assertEquals("ASCII code for r is :", "72", data[7].toHex)
+        Assert.assertEquals("72", data[10].toHex)
+    }
+
+    @Test fun testShortField() {
+        val field = Field.Short("myVar", 255)
+        val data = ByteArray(11)
+        field.getBytes(data, 0)
+        data.forEach { it.toInt().printHex() }
+        Assert.assertEquals("Container type should be: ", ContainerType.FIELD, data[0])
+        Assert.assertEquals("Name size should be: ", "myVar".length, data[2].toHex.toInt())
+        Assert.assertEquals("ASCII code for m is :", "6d", data[3].toHex)
+        Assert.assertEquals("ASCII code for y is :", "79", data[4].toHex)
+        Assert.assertEquals("ASCII code for V is :", "56", data[5].toHex)
+        Assert.assertEquals("ASCII code for a is :", "61", data[6].toHex)
+        Assert.assertEquals("ASCII code for r is :", "72", data[7].toHex)
+        Assert.assertEquals(255.toShort(), reader.readShort(data, 9))
+    }
 
 }
