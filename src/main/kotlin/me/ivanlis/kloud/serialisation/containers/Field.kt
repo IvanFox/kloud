7c41f6eb3fd74558c7efe3b374b0cb1c0c79f2d1
IvanFox <zvezdo4eet@gmail.com>
2016-10-13 17:45:06 +0100

    Data type is added to the serialised field

--- src/main/kotlin/me/ivanlis/kloud/serialisation/containers/Field.kt
+++ src/main/kotlin/me/ivanlis/kloud/serialisation/containers/Field.kt
@@ -2,7 +2,6 @@ package me.ivanlis.kloud.serialisation.containers
 
 import me.ivanlis.kloud.serialisation.SerialisationWriterImpl
 import me.ivanlis.kloud.serialisation.Type
-import me.ivanlis.kloud.serialisation.containers.ContainerType
 
 /**
  * Created by ivanlis on 07/10/2016.
@@ -13,7 +12,7 @@ abstract class Field(name: String) {
     val containerType = ContainerType.FIELD                 // 1 byte
     val name: ByteArray = name.toByteArray()
     val nameLength: kotlin.Short = name.length.toShort()    // 2 byte
-    var dataType: kotlin.Byte = 4                           // 1 byte
+    var dataType: kotlin.Byte = 0                          // 1 byte
     lateinit var data: ByteArray
 
     val writer = SerialisationWriterImpl()
@@ -28,11 +27,12 @@ abstract class Field(name: String) {
         return currentPointer
     }
 
-    fun getSize() : Int = 1 + 1 + 2 + name.size + data.size
+    fun getSize(): Int = 1 + 1 + 2 + name.size + data.size
 
 
     class Bool(name: String, value: Boolean) : Field(name) {
         init {
+            dataType = Type.BOOL
             data = ByteArray(Type.getSize(Type.BOOL))
             writer.writeBytes(data, 0, value)
         }
@@ -40,6 +40,7 @@ abstract class Field(name: String) {
 
     class Byte(name: String, value: kotlin.Byte) : Field(name) {
         init {
+            dataType = Type.BYTE
             data = ByteArray(Type.getSize(Type.BYTE))
             writer.writeBytes(data, 0, value)
         }
@@ -47,6 +48,7 @@ abstract class Field(name: String) {
 
     class Char(name: String, value: kotlin.Char) : Field(name) {
         init {
+            dataType = Type.CHAR
             data = ByteArray(Type.getSize(Type.CHAR))
             writer.writeBytes(data, 0, value)
         }
@@ -54,6 +56,7 @@ abstract class Field(name: String) {
 
     class Short(name: String, value: kotlin.Short) : Field(name) {
         init {
+            dataType = Type.SHORT
             data = ByteArray(Type.getSize(Type.SHORT))
             writer.writeBytes(data, 0, value)
         }
@@ -61,6 +64,7 @@ abstract class Field(name: String) {
 
     class Integer(name: String, value: Int) : Field(name) {
         init {
+            dataType = Type.INT
             data = ByteArray(Type.getSize(Type.INT))
             writer.writeBytes(data, 0, value)
         }
@@ -68,6 +72,7 @@ abstract class Field(name: String) {
 
     class Long(name: String, value: kotlin.Long) : Field(name) {
         init {
+            dataType = Type.LONG
             data = ByteArray(Type.getSize(Type.LONG))
             writer.writeBytes(data, 0, value)
         }
@@ -75,6 +80,7 @@ abstract class Field(name: String) {
 
     class Float(name: String, value: kotlin.Float) : Field(name) {
         init {
+            dataType = Type.FLOAT
             data = ByteArray(Type.getSize(Type.FLOAT))
             writer.writeBytes(data, 0, value)
         }
@@ -82,6 +88,7 @@ abstract class Field(name: String) {
 
     class Double(name: String, value: kotlin.Double) : Field(name) {
         init {
+            dataType = Type.DOUBLE
             data = ByteArray(Type.getSize(Type.DOUBLE))
             writer.writeBytes(data, 0, value)
         }
